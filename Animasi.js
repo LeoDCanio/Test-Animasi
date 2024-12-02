b9daefaea338fdbc61da486994d95695cbf2f279
Mireille Aliano <142092441+LeoDCanio@users.noreply.github.com>
2024-12-02 20:15:21 +0700

    Add files via upload

---
+++ Animasi.js
@@ -0,0 +1,5 @@
+function goToDashboard() {
+    // Menyembunyikan halaman login dan menampilkan dashboard dengan transisi yang diinginkan
+    document.getElementById("login").classList.remove("active");
+    document.getElementById("dashboard").classList.add("active");
+}
